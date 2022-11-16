86ee2f402288be2e5e36cf62fb6608a66885324e
ひとりごと <118036741+hitorigoto@users.noreply.github.com>
2022-11-16 20:38:49 +0700

    Update ez-gitconfig.bat

--- ez-gitconfig.bat
+++ ez-gitconfig.bat
@@ -1,17 +1,21 @@
 @echo off
 
-@echo █▀▀ ▀█   █▀▀ █ ▀█▀   █▀▀ █▀█ █▄░█ █▀▀ █ █▀▀
-@echo ██▄ █▄   █▄█ █ ░█░   █▄▄ █▄█ █░▀█ █▀░ █ █▄█ by P'Phurit
-@echo
+@echo  _____ _____   ____ ___ _____    ____ ___  _   _ _____ ___ ____ 
+@echo ^| ____^|__  /  / ___^|_ _^|_   _^|  / ___/ _ \^| \ ^| ^|  ___^|_ _/ ___^|
+@echo ^|  _^|   / /  ^| ^|  _ ^| ^|  ^| ^|   ^| ^|  ^| ^| ^| ^|  \^| ^| ^|_   ^| ^| ^|  _ 
+@echo ^| ^|___ / /_  ^| ^|_^| ^|^| ^|  ^| ^|   ^| ^|__^| ^|_^| ^| ^|\  ^|  _^|  ^| ^| ^|_^| ^|
+@echo ^|_____/____^|  \____^|___^| ^|_^|    \____\___/^|_^| \_^|_^|   ^|___\____^| by P'Phurit
+@echo:
 
 set /p name= "Enter user.name: "
 set /p email= "Enter user.email: "
 set /p msg= "Enter commit message: "
 
-@echo "Enter push commands from GitHub:"
+@echo Enter push commands from GitHub:
 set /p git1= ""
 set /p git2= ""
 set /p git3= ""
+@echo ---------------------------------------------------------------------------
 
 git init
 git config user.name "%name%"
@@ -23,6 +27,8 @@ git commit -m "%msg%"
 %git2%
 %git3%
 
-@echo "DONE!"
+@echo ----------------------------------------------------------------------------
+@echo DONE! ^<3
+@echo:
 
 pause
